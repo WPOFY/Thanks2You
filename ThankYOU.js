2fb187d121c087d415562a7d38d9fa4d33df824c
WPOFY <webprogrammingofy@gmail.com>
2023-09-05 19:23:21 +0700

    first commit

---
+++ ThankYOU.js
@@ -0,0 +1,26 @@
+function func(){
+    var Name = document.getElementById("Name").value;
+    if (Name == "Chels") {
+        alert("Succesfull ! ")
+        window.location.assign("https://wpofy.github.io/webprogrammingOFY4.github.io/");
+    }
+
+    if (Name == "Chelsea") {
+        alert("Succesfull ! ")
+        window.location.assign("https://wpofy.github.io/webprogrammingOFY4.github.io/");
+    }
+
+    if (Name == "Gabriella Chelsea") {
+        alert("Succesfull ! ")
+        window.location.assign("https://wpofy.github.io/webprogrammingOFY4.github.io/");
+    }
+
+    if (Name == "Gabriella Chelsea Andrea") {
+        alert("Succesfull ! ")
+        window.location.assign("https://wpofy.github.io/webprogrammingOFY4.github.io/");
+    }
+
+    else {
+        alert("Unidentified ! ")
+    }
+}
