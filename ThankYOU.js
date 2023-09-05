484c7b426654f40042aa966b2877a6e9fa1d2ae2
WPOFY <webprogrammingofy@gmail.com>
2023-09-05 19:27:49 +0700

    first commit

--- ThankYOU.js
+++ ThankYOU.js
@@ -2,22 +2,22 @@ function func(){
     var Name = document.getElementById("Name").value;
     if (Name == "Chels") {
         alert("Succesfull ! ")
-        window.location.assign("https://wpofy.github.io/webprogrammingOFY4.github.io/");
+        window.location.assign("https://github.com/WPOFY/Thanks2You.git");
     }
 
     if (Name == "Chelsea") {
         alert("Succesfull ! ")
-        window.location.assign("https://wpofy.github.io/webprogrammingOFY4.github.io/");
+        window.location.assign("https://github.com/WPOFY/Thanks2You.git");
     }
 
     if (Name == "Gabriella Chelsea") {
         alert("Succesfull ! ")
-        window.location.assign("https://wpofy.github.io/webprogrammingOFY4.github.io/");
+        window.location.assign("https://github.com/WPOFY/Thanks2You.git");
     }
 
     if (Name == "Gabriella Chelsea Andrea") {
         alert("Succesfull ! ")
-        window.location.assign("https://wpofy.github.io/webprogrammingOFY4.github.io/");
+        window.location.assign("https://github.com/WPOFY/Thanks2You.git");
     }
 
     else {
